00f391131e27a307fae39c7e6d7ddf95145f0a0b
orangedareoranges <159554889+orangedareoranges@users.noreply.github.com>
2024-02-11 11:35:18 +0000

    Actually publish the python script
    Dope.

---
+++ owlerpythonGITHUB.py
@@ -0,0 +1,14 @@
+import requests
+import json
+from base64 import b64encode // Shouldn't be necessary, but keep it still.
+
+print("Hey, wanna submit a status?:")
+x = input()
+print("Your status is: " + x)
+
+username = "INSERTUSERNAMEHERE"
+password = "INSERTPASSWORDHERE"
+requests.get("https://api.owler.cloud/v1/account/verify_credentials.json", auth=(username, password))
+url = "https://api.owler.cloud/v1/statuses/update.json"
+requests.post(f"{url}?status={x}", auth=(username, password))
+print("Success! Check the public timeline.")
